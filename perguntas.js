29a9219025562fc87e258f164f613235de7844ff
bolhasss <128045911+bolhasss@users.noreply.github.com>
2024-09-18 07:42:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartão (
+    ‘Filosofia’,
+    ‘Quem criou o marxismo’,
+    ‘Karl Marx’)
+    
+    criaCartão (
+    ‘História’,
+    ‘Quem descobriu o Brasil’,
+    ‘Pedro Álvares Cabral’)
+    
+    criaCartão (
+    ‘Arte’,
+    ‘Quem criou a escultura de David na igreja da Itália’,
+    ‘Michelangelo’)
+    
+    criaCartão (
+    ‘Matemática’,
+    ‘Qual a fórmula da razão’,
+    ‘Numerador/Denominador’)
+    
+    criaCartão (
+    ‘Arte’,
+    ‘Quem criou a Mona Lisa’,
+    ‘Leonardo Da Vinci’)
+    
+    criaCartão (
+    ‘História’,
+    ‘Aonde começou o fascismo’,
+    ‘Itália’)
+    
+    criaCartão (
+    ‘História’,
+    ‘Qual era os pilar principal para a ideologia do nazismo’,
+    ‘antissemitismo(ódio de judeus)’)
+    
+    criaCartão (
+    ‘Biologia’,
+    ‘O que estuda as células’,
+    ‘Citologia’)
+    
+    criaCartão (
+    ‘Arte’,
+    ‘Porque Van Gogh cortou sua própria orelha’,
+    ‘Briga em bar enquanto estava muito bravo’)
+    
+    criaCartão (
+    ‘Filosofia’,
+    ‘O que é ética’,
+    ‘princípios que orientam o comportamento humano, determinando o que é certo e errado’)
+    
+    criaCartão (
+    ‘História’,
+    ‘Quando começou e terminou a Segunda Guerra Mundial’,
+    ‘1939-1945’)
+    
+    criaCartão (
+    ‘Filosofia’,
+    ‘O que foi o período socrático’,
+    ‘estudo da ética, política e conhecimento’)
+    
+    criaCartão (
+    ‘Arte’,
+    ‘Qual artista criou o primeiro rascunho de um helicóptero’,
+    ‘Leonardo Da Vinci’)
+    
+    criaCartão (
+    ‘Filosofia’,
+    ‘Qual conceito Aristóteles é conhecido por’,
+    ‘Que a felicidade é o fim da vida humana, alcançada pela virtude’)
+    
+    criaCartão (
+    ‘História’,
+    ‘Quantos anos demorou a Primeira Guerra Mundial’,
+    ‘4 anos, de 1914-1918’)
+    
+    criaCartão (
+    ‘História’,
+    ‘Antes da Revolução Russa, ela era conhecida por ser um país’,
+    ‘Agrário, feudal, dominada pelo clero e por imperadores’)
+    
+    criaCartão (
+    ‘Arte’,
+    ‘Quem pintou “The Last Supper”’,
+    ‘Leonardo Da Vinci’)
+    
+    criaCartão (
+    ‘Biologia’,
+    ‘Quantos cromossomos tem uma pessoa’,
+    ‘46’)
+    
+    criaCartão (
+    ‘Física’,
+    ‘Qual a medida de comprimento em S.I.’,
+    ‘Centímetro’)
+    
+    criaCartão (
+    ‘Biologia’,
+    ‘Quantos cromossomos tem uma pessoa com síndrome de down’,
+    ‘42’)
